dfcb4dbae6df7275b1fc68a3a924da21e07502de
geek-nath <nath.codes@gmail.com>
2023-03-07 05:25:06 +0000

    ui debugging

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ window.addEventListener("load", function() {
       loader.style.display = "none";
     }, 3000);
 });
+
+// show socila links on scroll
+
+const socialLinkBlock = document.getElementById('socialBlock');
+
+window.onscroll = ()=> {
+    if(window.scrollY > 2) {
+        socialLinkBlock.classList.add('social_block_show');
+    } else {
+        socialLinkBlock.classList.remove('social_block_show');
+    }
+}
